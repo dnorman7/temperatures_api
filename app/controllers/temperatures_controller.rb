35174b3eb9319c9b1f5ecb5689714facd72b3040
Deanna Norman <deannaleenorman@msn.com>
2021-01-04 08:45:41 -0800

    removes show, destroy, and update actions from temperatures controller

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,16 +1,10 @@
 class TemperaturesController < ApplicationController
-  before_action :set_temperature, only: [:show, :update, :destroy]
 
   # GET /temperatures
   def index
     @temperatures = Temperature.all
 
     render json: @temperatures
-  end
-
-  # GET /temperatures/1
-  def show
-    render json: @temperature
   end
 
   # POST /temperatures
@@ -24,26 +18,7 @@ class TemperaturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /temperatures/1
-  def update
-    if @temperature.update(temperature_params)
-      render json: @temperature
-    else
-      render json: @temperature.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /temperatures/1
-  def destroy
-    @temperature.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_temperature
-      @temperature = Temperature.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def temperature_params
       params.require(:temperature).permit(:average_high_f, :average_low_f, :month)
